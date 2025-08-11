16d45b230e06f9c2f1158efcb5f0257b765d7e2a
Vishnu Syamala <vvishnusyamala@gmail.com>
2025-08-11 14:33:52 +0530

    vdffgdf

--- backend/server.js
+++ backend/server.js
@@ -11,25 +11,14 @@ const bookingRoutes = require('./routes/bookingRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const allowedOrigins = [
-  'https://event-taupe-two.vercel.app'
-];
+
 connectDB().catch(err => { console.error('DB connection failed', err); process.exit(1); });
 
 app.use(helmet());
 
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps, curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    return callback(new Error('Not allowed by CORS'));
-  },
-  credentials: true
-}));
+app.use(cors());
+
 
 app.use(express.json());
 
